d64d76d5a8dfc28f048cc9e0d08051087a900d1d
Timiry <irina.v.tim@gmail.com>
2025-10-17 14:30:20 +0300

    create fsd structure and main routes

---
+++ src/app/confirm-email/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Подтверждение email",
+}
+
+export default function ConfirmEmail() {
+  return (
+    <div className="font-sans grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      ConfirmEmail
+    </div>
+  );
+}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Регистрация",
+}
+
+export default function Register() {
+  return (
+    <div className="font-sans grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      Register
+    </div>
+  );
+}
