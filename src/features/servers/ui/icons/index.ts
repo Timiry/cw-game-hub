571210b6d81a946b30a75751d5140c0d5be06f0d
Timiry <irina.v.tim@gmail.com>
2025-12-03 17:55:01 +0300

    add icons for ServerListPage

---
+++ src/features/servers/ui/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as AddIcon } from "./AddIcon";
+export { default as NetworkLoader } from "./NetworkLoaderIcon";
+export { default as BlockIcon } from "./BlockIcon";
+export { default as DisconnectIcon } from "./DisconnectIcon";
+export { default as RunningCatIcon } from "./RunningCatIcon";
