4908cb8b0dd638fb86a6f9a3ce3a0b2802dac731
Timiry <irina.v.tim@gmail.com>
2026-06-28 22:57:31 +0300

    add breadcrumbs on legal docks pages

--- src/pages/legalPrivacy/index.tsx
+++ src/pages/legalPrivacy/index.tsx
@@ -6,39 +6,32 @@ import privacyEn from "@/shared/consts/legalDocs/privacyEn";
 import privacyRu from "@/shared/consts/legalDocs/privacyRu";
 import SafeHtml from "@/shared/ui/SafeHtml";
 import Footer from "@/widgets/Footer";
-import { Paper, Button } from "@cw-game/react-ui";
-import { useRouter } from "@/shared/i18n/navigation";
+import { Paper, Typography, Link } from "@cw-game/react-ui";
+import NextLink from "next/link";
 import routes from "@/shared/config/routes";
 
 const LegalPrivacyPage = () => {
   const t = useTranslations("LegalPage");
   const locale = useLocale();
-  const router = useRouter();
 
   return (
     <>
       <Box m="80px auto" width="80%" maxWidth="1200px">
+        <Typography>
+          <Link component={NextLink} href={routes.legal}>
+            {t("legalDocs")}
+          </Link>
+          {" > "}
+          {t("privacy")}
+        </Typography>
         <Paper
           component={Box}
           p={{ mobile: 1, tablet: 3 }}
           overflow="auto"
-          mb={3}
+          my={3}
         >
           <SafeHtml html={locale === "ru" ? privacyRu : privacyEn} />
         </Paper>
-        <Button
-          variant="outlined"
-          onClick={() => router.push(routes.legal)}
-          sx={{
-            display: "block",
-            margin: {
-              mobile: "0 auto",
-              desktop: "0",
-            },
-          }}
-        >
-          {t("toLegalDocsButton")}
-        </Button>
       </Box>
       <Footer />
     </>
--- src/pages/legalTerms/index.tsx
+++ src/pages/legalTerms/index.tsx
@@ -6,39 +6,32 @@ import { useLocale, useTranslations } from "next-intl";
 import termsEn from "@/shared/consts/legalDocs/termsEn";
 import termsRu from "@/shared/consts/legalDocs/termsRu";
 import Footer from "@/widgets/Footer";
-import { Paper, Button } from "@cw-game/react-ui";
+import { Paper, Typography, Link } from "@cw-game/react-ui";
+import NextLink from "next/link";
 import routes from "@/shared/config/routes";
-import { useRouter } from "@/shared/i18n/navigation";
 
 const LegalTermsPage = () => {
   const t = useTranslations("LegalPage");
   const locale = useLocale();
-  const router = useRouter();
 
   return (
     <>
       <Box m="80px auto" width="80%" maxWidth="1200px">
+        <Typography>
+          <Link component={NextLink} href={routes.legal}>
+            {t("legalDocs")}
+          </Link>
+          {" > "}
+          {t("terms")}
+        </Typography>
         <Paper
           component={Box}
           p={{ mobile: 1, tablet: 3 }}
           overflow="auto"
-          mb={3}
+          my={3}
         >
           <SafeHtml html={locale === "ru" ? termsRu : termsEn} />
         </Paper>
-        <Button
-          variant="outlined"
-          onClick={() => router.push(routes.legal)}
-          sx={{
-            display: "block",
-            margin: {
-              mobile: "0 auto",
-              desktop: "0",
-            },
-          }}
-        >
-          {t("toLegalDocsButton")}
-        </Button>
       </Box>
       <Footer />
     </>
